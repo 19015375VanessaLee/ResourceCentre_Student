f037ed63c0505b7988611205b6ce5575280690f5
19015375 <19015375@DESKTOP-JP1C48R>
2020-07-24 13:34:38 +0800

    add

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -171,8 +171,11 @@ public class ResourceCentre {
 	}
 	
 	public static Chromebook inputChromebook() {	
-		Chromebook cb =null;
 		// write your code here
+		String tag = Helper.readString("Enter asset tag > ");
+		String description = Helper.readString("Enter description > ");
+		
+		Chromebook cb = new Chromebook(tag, description, null);
 		return cb;
 		
 	}	
